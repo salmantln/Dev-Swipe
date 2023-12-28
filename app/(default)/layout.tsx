37c643b4660c424b4034d80360f74f8dfeeaabb0
salmantln <50806616+salmantln@users.noreply.github.com>
2023-12-28 12:24:56 +0100

    Added: added padding

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -25,7 +25,7 @@ export default function DefaultLayout({
 
   return (
     <>
-      <main className="grow">
+      <main className="grow p-16">
         {/* <PageIllustration /> */}
 
         {children}
