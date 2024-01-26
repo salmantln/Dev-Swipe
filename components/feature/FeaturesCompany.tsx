ed1c419a9994e1ad88334fa56be9a7b7e4dffd0f
salmantln <50806616+salmantln@users.noreply.github.com>
2024-01-26 16:07:19 +0100

    update:poging 4

--- components/feature/FeaturesCompany.tsx
+++ components/feature/FeaturesCompany.tsx
@@ -8,15 +8,15 @@ const FeaturesCompany = () => {
         Why choosing DevSwipe for your Company?
       </h2>
       <div className="mt-12 grid grid-cols-1 md:grid-cols-3 gap-x-8 gap-y-4 items-center justify-items-center mx-auto max-w-4xl">
-        <Image className="md:order-1" width={100} height={100} src="/img/potential.svg" alt="" />
+        <Image className="md:order-1" width={100} height={100} src="/img/potential.svg" alt="feature_1" />
         <h4 className="md:order-4 font-semibold text-xl text-primary">
           10.000+
         </h4>
         <p className="md:order-7 self-start text-white">Potential candidates</p>
-        <Image className="md:order-2" width={100} height={100}  src="/img/enterprise.svg" alt="" />
+        <Image className="md:order-2" width={100} height={100}  src="/img/enterprise.svg" alt="feature_2" />
         <h4 className="md:order-5 font-semibold text-xl text-accent">120+</h4>
         <p className="md:order-8 self-start text-white">Joined Companies</p>
-        <Image className="md:order-3" width={100} height={100}  src="/img/matches.svg" alt="" />
+        <Image className="md:order-3" width={100} height={100}  src="/img/matches.svg" alt="feature_3" />
         <h4 className="md:order-6 font-semibold text-xl text-white">200+</h4>
         <p className="md:order-9 self-start text-white">Monthly matches</p>
       </div>
