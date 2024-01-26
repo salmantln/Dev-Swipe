27c6b7a8f954e43e282e4e455f344536cb745da7
salmantln <50806616+salmantln@users.noreply.github.com>
2024-01-26 15:50:04 +0100

    update:poging 3

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,15 @@
-const siteMetadata = require ('./siteMetadata');
+// const siteMetadata = require ('./siteMetadata');
 
+// module.exports = {
+//     siteUrl: siteMetadata.siteUrl,
+//     generateRobotsTxt: true,
+//     sitemapSize: 7000,
+//   }
+
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: siteMetadata.siteUrl,
+    siteUrl: 'https://dev-swipe.com',
     generateRobotsTxt: true,
+    generateIndexSitemap: false,
     sitemapSize: 7000,
   }
