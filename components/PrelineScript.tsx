ad8e97a7baa9717bbc25447064e9c78c2a6291b6
salmantln <50806616+salmantln@users.noreply.github.com>
2024-02-11 18:30:22 +0100

    Update:  Changed preline component

--- components/PrelineScript.tsx
+++ components/PrelineScript.tsx
@@ -1,41 +1,12 @@
-// "use client";
-
-// import { usePathname } from "next/navigation";
-// import { useEffect } from "react";
-
-
-// declare let HSStaticMethods: {
-//   autoInit(collection?: string | string[]): void;
-// };
-
-// export default function PrelineScript() {
-//   const path = usePathname();
-
-//   useEffect(() => {
-//     import("preline/preline");
-//   }, []);
-
-//   useEffect(() => {
-//     setTimeout(() => {
-//       HSStaticMethods.autoInit();
-//     }, 100);
-//   }, [path]);
-
-//   return null;
-// }
-
-
 "use client";
 
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 
-import { IStaticMethods } from "preline/preline";
-declare global {
-  interface Window {
-    HSStaticMethods: IStaticMethods;
-  }
-}
+
+declare let HSStaticMethods: {
+  autoInit(collection?: string | string[]): void;
+};
 
 export default function PrelineScript() {
   const path = usePathname();
@@ -46,9 +17,39 @@ export default function PrelineScript() {
 
   useEffect(() => {
     setTimeout(() => {
-      window.HSStaticMethods.autoInit();
+      HSStaticMethods.autoInit();
     }, 100);
   }, [path]);
 
   return null;
 }
+
+
+// "use client";
+
+// import { usePathname } from "next/navigation";
+// import { useEffect } from "react";
+
+// import { IStaticMethods } from "preline/preline";
+// // Correct usage if IStaticMethods is the desired type
+// declare global {
+//   interface Window {
+//     HSStaticMethods: IStaticMethods;
+//   }
+// }
+
+// export default function PrelineScript() {
+//   const path = usePathname();
+
+//   useEffect(() => {
+//     import("preline/preline");
+//   }, []);
+
+//   useEffect(() => {
+//     setTimeout(() => {
+//       window.HSStaticMethods.autoInit();
+//     }, 100);
+//   }, [path]);
+
+//   return null;
+// }
