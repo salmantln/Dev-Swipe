5185df6355e1e4db0625e29f7afa65dbe825a825
salmantln <50806616+salmantln@users.noreply.github.com>
2024-02-11 18:35:47 +0100

    Update:  All should be fine. Removed card in components

--- components/faq/FaqCompany.tsx
+++ components/faq/FaqCompany.tsx
@@ -1,5 +1,5 @@
 import { Disclosure } from "@headlessui/react";
-import { ChevronUpIcon } from "@heroicons/react/20/solid";
+import { ChevronUpIcon } from "@heroicons/react/solid";
 
 import Container from "../hero/container";
 
