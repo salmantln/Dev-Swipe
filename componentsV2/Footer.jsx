719dffd95e66ff8d32e60b225e0ef3210b52c55b
salmantln <50806616+salmantln@users.noreply.github.com>
2024-02-18 12:52:47 +0100

    Added: made sure to receive feedback if signed to the newsletter

--- componentsV2/Footer.jsx
+++ componentsV2/Footer.jsx
@@ -1,12 +1,15 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
 
-import { Button } from '@/componentsV2/Button'
-import { Container } from '@/componentsV2/Container'
-import { TextField } from '@/componentsV2/Fields'
-import { Logomark } from '@/componentsV2/Logo'
-import { NavLinks } from '@/componentsV2/NavLinks'
-import qrCode from '@/images/qr-code.svg'
+import { Button } from "@/componentsV2/Button";
+import { Container } from "@/componentsV2/Container";
+import { TextField } from "@/componentsV2/Fields";
+import { Logomark } from "@/componentsV2/Logo";
+import { NavLinks } from "@/componentsV2/NavLinks";
+import qrCode from "@/images/qr-code.svg";
+import { cn } from "@/utils/cn";
+
+import { useState } from "react";
 
 function QrCodeBorder(props) {
   return (
@@ -17,12 +20,55 @@ function QrCodeBorder(props) {
         strokeLinecap="round"
       />
     </svg>
-  )
+  );
 }
 
 export function Footer() {
+  const [email, setEmail] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-  
+  async function handleSubmit(event) {
+    event.preventDefault(); // Prevent default form submission behavior
+
+    try {
+      setIsSubmitting(true); // Set loading state
+
+      const res = await fetch("/api/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+        }),
+      });
+
+      if (res.ok) {
+        // Show success message
+        setSuccess(true);
+        setEmail(""); // Reset email field
+        toast.success("You have successfully subscribed!");
+      } else {
+        // Show error message
+        toast.error(
+          "An error occurred while subscribing. Please try again later."
+        );
+      }
+    } catch (error) {
+      // Show error message
+      toast.error(
+        "An error occurred while subscribing. Please try again later."
+      );
+    } finally {
+      setIsSubmitting(false); // Reset loading state
+    }
+  }
+
+  function handleChange(event) {
+    setEmail(event.target.value);
+  }
+
   return (
     <footer className="border-t border-gray-200">
       <Container>
@@ -42,7 +88,11 @@ export function Footer() {
           <div className="group relative -mx-4 flex items-center self-stretch p-4 transition-colors hover:bg-gray-100 sm:self-auto sm:rounded-2xl lg:mx-0 lg:self-auto lg:p-6">
             <div className="relative flex h-24 w-24 flex-none items-center justify-center">
               <QrCodeBorder className="absolute inset-0 h-full w-full stroke-gray-300 transition-colors group-hover:stroke-cyan-500" />
-              <Image src={qrCode} alt="DevSwipe QR Playstore Apple Store" unoptimized />
+              <Image
+                src={qrCode}
+                alt="DevSwipe QR Playstore Apple Store"
+                unoptimized
+              />
             </div>
             <div className="ml-8 lg:w-64">
               <p className="text-base font-semibold text-gray-900">
@@ -52,13 +102,19 @@ export function Footer() {
                 </Link>
               </p>
               <p className="mt-1 text-sm text-gray-700">
-                Scan the QR code to download the app from the App Store & Play Store.
+                Scan the QR code to download the app from the App Store & Play
+                Store.
               </p>
             </div>
           </div>
         </div>
         <div className="flex flex-col items-center border-t border-gray-200 pb-12 pt-8 md:flex-row-reverse md:justify-between md:pt-6">
-          <form className="flex w-full justify-center md:w-auto">
+          <form
+            onSubmit={handleSubmit}
+            className={cn(
+              "newsletter-form is-revealing  flex-col gap-2 sm:flex-row flex w-full justify-center md:w-auto"
+            )}
+          >
             <TextField
               type="email"
               aria-label="Email address"
@@ -66,11 +122,18 @@ export function Footer() {
               autoComplete="email"
               required
               className="w-60 min-w-0 shrink"
+              value={email}
+              onChange={handleChange}
             />
             <Button type="submit" color="cyan" className="ml-4 flex-none">
               <span className="hidden lg:inline">Join our newsletter</span>
               <span className="lg:hidden">Join newsletter</span>
             </Button>
+            {success && (
+              <div className="mt-2 text-xs italic text-gray-500">
+                Email submitted successfully!
+              </div>
+            )}
           </form>
           <p className="mt-6 text-sm text-gray-500 md:mt-0">
             &copy; Copyright {new Date().getFullYear()}. All rights reserved.
@@ -78,5 +141,5 @@ export function Footer() {
         </div>
       </Container>
     </footer>
-  )
+  );
 }
