cea4e83d1dccf9fb7aafdb1b5a4b28ab649091ff
salmantln <50806616+salmantln@users.noreply.github.com>
2024-02-19 08:57:06 +0100

    Added: Next dynamic import

--- app/test/job-post/page.tsx
+++ app/test/job-post/page.tsx
@@ -1,10 +1,10 @@
 // import React from 'react'
-import ClientComponent from '../../../dashboard_components/client_components'
+// import ClientComponent from '../../../dashboard_components/client_components'
 import dynamic from "next/dynamic";
 import { useState, useMemo } from "react";
 
 export default function  JobPostPage () {
-  const ClientComponent = useMemo(() => dynamic(() => import('react-quill'), { ssr: false }),[]);
+  const ClientComponent = useMemo(() => dynamic(() => import('../../../dashboard_components/client_components'), { ssr: false }),[]);
 
   return (
     <>
