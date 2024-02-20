fab6063fe38b5374a2f3b42e837948440ce0bd11
salmantln <50806616+salmantln@users.noreply.github.com>
2024-02-20 14:34:11 +0100

    Added featured in hackernews

--- componentsV2/Hero.jsx
+++ componentsV2/Hero.jsx
@@ -353,6 +353,8 @@ export function Hero() {
   }
 
   return (
+
+    
     <div className="overflow-hidden py-20 sm:py-32 lg:pb-32 xl:pb-36">
       <Container>
         <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20">
