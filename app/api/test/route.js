dc966d9580e5e763de3352e4eddc6ea4ab4d3732
salmantln <50806616+salmantln@users.noreply.github.com>
2024-03-01 20:30:00 +0100

    Updated: redeploy for env

--- app/api/test/route.js
+++ app/api/test/route.js
@@ -3,49 +3,59 @@ import { NextResponse } from "next/server";
 import puppeteer from "puppeteer";
 
 export async function GET() {
-  try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    const url =
-      "https://rojoconsultancy.recruitee.com/o/enterprise-integration-consultant-spain"; // Replace with the URL of the web page
+  // try {
+  //   const browser = await puppeteer.launch();
+  //   const page = await browser.newPage();
+  //   const url =
+  //     "https://rojoconsultancy.recruitee.com/o/enterprise-integration-consultant-spain"; // Replace with the URL of the web page
 
-    await page.goto(url);
+  //   await page.goto(url);
 
-    const cityValue = await page.evaluate(() => {
-      const element = document.querySelector(".custom-css-style-job-location-city");
-      return element ? element.textContent.trim() : "";
-    });
+  //   const cityValue = await page.evaluate(() => {
+  //     const element = document.querySelector(".custom-css-style-job-location-city");
+  //     return element ? element.textContent.trim() : "";
+  //   });
 
-    const countryValue = await page.evaluate(() => {
-      const element = document.querySelector(".custom-css-style-job-location-country");
-      return element ? element.textContent.trim() : "";
-    });
+  //   const countryValue = await page.evaluate(() => {
+  //     const element = document.querySelector(".custom-css-style-job-location-country");
+  //     return element ? element.textContent.trim() : "";
+  //   });
 
-    const jobTitleValue = await page.evaluate(() => {
-      const element = document.querySelector(".sc-crgk9f-2");
-      return element ? element.textContent.trim() : "";
-    });
+  //   const jobTitleValue = await page.evaluate(() => {
+  //     const element = document.querySelector(".sc-crgk9f-2");
+  //     return element ? element.textContent.trim() : "";
+  //   });
 
-    const contentHTML = await page.evaluate(() => {
-      const element = document.querySelector(".sc-1v95195-0");
-      return element ? element.innerHTML : "";
-    });
+  //   const contentHTML = await page.evaluate(() => {
+  //     const element = document.querySelector(".sc-1v95195-0");
+  //     return element ? element.innerHTML : "";
+  //   });
 
-    const cleanHTML = contentHTML.replace(/(\sclass=\")[^\"]*(\")/g, "").replace(/<(\w+)(\s[^>]*)?>/g, "<$1>"); // Remove class attributes and other attributes from tags
+  //   const cleanHTML = contentHTML.replace(/(\sclass=\")[^\"]*(\")/g, "").replace(/<(\w+)(\s[^>]*)?>/g, "<$1>"); // Remove class attributes and other attributes from tags
 
-    console.log("City:", cityValue);
-    console.log("Country:", countryValue);
-    console.log("Title:", jobTitleValue);
-    console.log("Content HTML:", cleanHTML);
+  //   console.log("City:", cityValue);
+  //   console.log("Country:", countryValue);
+  //   console.log("Title:", jobTitleValue);
+  //   console.log("Content HTML:", cleanHTML);
 
-    await browser.close();
+  //   await browser.close();
 
-    return NextResponse.json({
-      location: cityValue + " " + countryValue,
-      jobTitle: jobTitleValue,
-      contentHTML: cleanHTML,
-    });
-  } catch (error) {
-    return new Response("Check field and try again.", { status: 400, error });
-  }
+  //   return NextResponse.json({
+  //     location: cityValue + " " + countryValue,
+  //     jobTitle: jobTitleValue,
+  //     contentHTML: cleanHTML,
+  //   });
+  // } catch (error) {
+  //   return new Response("Check field and try again.", { status: 400, error });
+  // }
+
+  const res = await fetch('https://rojoconsultancy.recruitee.com/o/enterprise-integration-consultant-spain', {
+    headers: {
+      'Content-Type': 'application/json',
+      // 'API-Key': process.env.DATA_API_KEY,
+    },
+  })
+  const data = await res.json()
+ 
+  return Response.json({ data })
 }
